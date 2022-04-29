b6b0d9f7b17942836fa9b03d8b6e2d923547df89
Sonam Gupta <2997sonamgeda@gmail.com>
2022-04-29 12:43:56 +0530

    we used controller,service and service implementation,DAO and Get method

--- src/main/java/com/dlithe/bankingapp/service/TestService.java
+++ src/main/java/com/dlithe/bankingapp/service/TestService.java
@@ -1,6 +1,7 @@
 package com.dlithe.bankingapp.service;
 
 import com.dlithe.bankingapp.DTO.CustomerDetailRequest;
+import com.dlithe.bankingapp.DTO.UserDetails;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,4 +9,6 @@ public interface TestService {
     String productDetails(String productName);
 
     String registerCustomer(CustomerDetailRequest customerDetailRequest);
+
+    UserDetails getUserDetails(int userId);
 }
--- src/main/java/com/dlithe/bankingapp/serviceimplementation/TestServiceImpl.java
+++ src/main/java/com/dlithe/bankingapp/serviceimplementation/TestServiceImpl.java
@@ -2,11 +2,23 @@ package com.dlithe.bankingapp.serviceimplementation;
 
 
 import com.dlithe.bankingapp.DTO.CustomerDetailRequest;
+import com.dlithe.bankingapp.DTO.UserDetails;
+import com.dlithe.bankingapp.entity.Login;
+import com.dlithe.bankingapp.repository.LoginDAO;
 import com.dlithe.bankingapp.service.TestService;
+import com.sun.deploy.cache.BaseLocalApplicationProperties;
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class TestServiceImpl implements TestService {
+
+
+    @Autowired
+    private LoginDAO loginDAO;
 
     @Override
     public String productDetails(String productName) {
@@ -32,10 +44,26 @@ public class TestServiceImpl implements TestService {
     @Override
     public String registerCustomer(CustomerDetailRequest customerDetailRequest) {
 
+        Login login = new Login();
+        login.setName(customerDetailRequest.getName());
+        login.setEmail(customerDetailRequest.getEmail());
+        login.setPassword(customerDetailRequest.getPassword());
 
-        System.out.println(customerDetailRequest);
-        return null;
+
+        loginDAO.save(login);
+        return "User Saved Successfully";
     }
 
 
+    @Override
+    public UserDetails getUserDetails(int userId) {
+        Optional<Login> logins = loginDAO.findById(userId);
+        Login login = logins.get();
+        UserDetails userDetails = new UserDetails();
+        userDetails.setName(login.getName());
+        userDetails.setEmail(login.getEmail());
+        userDetails.setPassword(login.getPassword());
+        return userDetails;
+
+    }
 }
